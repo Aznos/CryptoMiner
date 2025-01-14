fd821db6d1914c1c18fdd9129fc3b726f088497c
aznos <pigiotyreal@gmail.com>
2025-01-14 11:43:33 -0500

    Setup

---
+++ src/main/java/com/aznos/crypto/Crypto.java
@@ -0,0 +1,10 @@
+package com.aznos.crypto;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Crypto extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+}
