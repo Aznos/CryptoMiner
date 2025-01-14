e6a1f48b9fbedeef2f586e4ffd383940ae2a70c6
aznos <pigiotyreal@gmail.com>
2025-01-14 13:44:21 -0500

    Revenue!

--- src/main/java/com/aznos/crypto/Crypto.java
+++ src/main/java/com/aznos/crypto/Crypto.java
@@ -1,6 +1,11 @@
 package com.aznos.crypto;
 
 import com.aznos.crypto.command.CryptoCommand;
+import com.aznos.crypto.data.PlayerData;
+import com.aznos.crypto.data.miners.GT1030;
+import com.aznos.crypto.db.Database;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.sql.SQLException;
@@ -16,6 +21,27 @@ public final class Crypto extends JavaPlugin {
         INSTANCE = this;
         makeDataFolder();
         initDB();
+
+        Bukkit.getScheduler().runTaskTimer(this, () -> {
+            for(Player player : Bukkit.getOnlinePlayers()) {
+                double revenueEarnedFromMiners = 0;
+
+                PlayerData data = Database.fetchPlayerData(player.getUniqueId());
+                String inventory = data.inventory();
+
+                for(String miner : inventory.trim().split(",")) {
+                    if(miner.equalsIgnoreCase("GT-1030")) {
+                        data = new PlayerData(inventory, data.crypto() + GT1030.getRevenue());
+                        Database.savePlayerData(player.getUniqueId(), data.inventory(), data.crypto());
+                        revenueEarnedFromMiners += GT1030.getRevenue();
+                    }
+                }
+
+                if(revenueEarnedFromMiners > 0) {
+                    player.sendMessage("You have earned " + revenueEarnedFromMiners + "₿ from your miners");
+                }
+            }
+        }, 24000, 24000);
 
         getCommand("crypto").setExecutor(new CryptoCommand());
     }
