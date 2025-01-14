88fcc008b7d31d3b7c52165c10a83f7348e67b49
aznos <pigiotyreal@gmail.com>
2025-01-14 12:44:15 -0500

    Put database in another file

--- src/main/java/com/aznos/crypto/Crypto.java
+++ src/main/java/com/aznos/crypto/Crypto.java
@@ -1,16 +1,18 @@
 package com.aznos.crypto;
 
-import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.sql.*;
-import java.util.UUID;
+import java.sql.SQLException;
+
+import static com.aznos.crypto.db.Database.connection;
+import static com.aznos.crypto.db.Database.initDB;
 
 public final class Crypto extends JavaPlugin {
-    private Connection connection;
+    public static Crypto INSTANCE;
 
     @Override
     public void onEnable() {
+        INSTANCE = this;
         initDB();
     }
 
@@ -22,63 +24,6 @@ public final class Crypto extends JavaPlugin {
             } catch(SQLException e) {
                 getLogger().warning("Failed to close database connection");
             }
-        }
-    }
-
-    private void initDB() {
-        try {
-            connection = DriverManager.getConnection("jdbc:sqlit:" + getDataFolder() + "/data.db");
-
-            String createTableQuery = """
-                    CREATE TABLE IF NOT EXISTS player_data (
-                        id TEXT PRIMARY KEY,
-                        inventory TEXT,
-                        crypto REAL
-                    );
-            """;
-
-            try(PreparedStatement stmt = connection.prepareStatement(createTableQuery)) {
-                stmt.executeUpdate();
-            }
-
-            getLogger().info("Database connection established");
-        } catch(SQLException e) {
-            getLogger().warning("Failed to establish database connection");
-        }
-    }
-
-    public void savePlayerData(UUID uuid, String inventory, double crypto) {
-        String id = uuid.toString();
-        String insertQuery = """
-            INSERT OR REPLACE INTO player_data (id, inventory, crypto)
-            VALUES (?, ?, ?);
-        """;
-
-        try(PreparedStatement stmt = connection.prepareStatement(insertQuery)) {
-            stmt.setString(1, id);
-            stmt.setString(2, inventory);
-            stmt.setDouble(3, crypto);
-        } catch(SQLException e) {
-            getLogger().severe("Failed to save player data for " + Bukkit.getPlayer(uuid));
-        }
-    }
-
-    public void fetchPlayerData(UUID uuid) {
-        String id = uuid.toString();
-        String selectQuery = "SELECT inventory, crypto FROM player_data WHERE id = ?";
-
-        try(PreparedStatement stmt = connection.prepareStatement(selectQuery)) {
-            stmt.setString(1, id);
-            ResultSet rs = stmt.executeQuery();
-
-            if(rs.next()) {
-                String inventory = rs.getString("inventory");
-                double crypto = rs.getDouble("crypto");
-            } else {
-                getLogger().warning("No data found for player " + Bukkit.getPlayer(uuid) + ", yet it was requested");
-            }
-        } catch(SQLException e) {
-            getLogger().severe("Failed to fetch player data for " + Bukkit.getPlayer(uuid));
         }
     }
 }
---
+++ src/main/java/com/aznos/crypto/db/Database.java
@@ -0,0 +1,68 @@
+package com.aznos.crypto.db;
+
+import com.aznos.crypto.Crypto;
+import org.bukkit.Bukkit;
+
+import java.sql.*;
+import java.util.UUID;
+
+public class Database {
+    public static Connection connection;
+
+    public static void initDB() {
+        try {
+            connection = DriverManager.getConnection("jdbc:sqlite:" + Crypto.INSTANCE.getDataFolder() + "/data.db");
+
+            String createTableQuery = """
+                    CREATE TABLE IF NOT EXISTS player_data (
+                        id TEXT PRIMARY KEY,
+                        inventory TEXT,
+                        crypto REAL
+                    );
+            """;
+
+            try(PreparedStatement stmt = connection.prepareStatement(createTableQuery)) {
+                stmt.executeUpdate();
+            }
+
+            Bukkit.getLogger().info("Database connection established");
+        } catch(SQLException e) {
+            Bukkit.getLogger().warning("Failed to establish database connection");
+        }
+    }
+
+    public static void savePlayerData(UUID uuid, String inventory, double crypto) {
+        String id = uuid.toString();
+        String insertQuery = """
+            INSERT OR REPLACE INTO player_data (id, inventory, crypto)
+            VALUES (?, ?, ?);
+        """;
+
+        try(PreparedStatement stmt = connection.prepareStatement(insertQuery)) {
+            stmt.setString(1, id);
+            stmt.setString(2, inventory);
+            stmt.setDouble(3, crypto);
+        } catch(SQLException e) {
+            Bukkit.getLogger().severe("Failed to save player data for " + Bukkit.getPlayer(uuid));
+        }
+    }
+
+    public static void fetchPlayerData(UUID uuid) {
+        String id = uuid.toString();
+        String selectQuery = "SELECT inventory, crypto FROM player_data WHERE id = ?";
+
+        try(PreparedStatement stmt = connection.prepareStatement(selectQuery)) {
+            stmt.setString(1, id);
+            ResultSet rs = stmt.executeQuery();
+
+            if(rs.next()) {
+                String inventory = rs.getString("inventory");
+                double crypto = rs.getDouble("crypto");
+            } else {
+                Bukkit.getLogger().warning("No data found for player " + Bukkit.getPlayer(uuid) + ", yet it was requested");
+            }
+        } catch(SQLException e) {
+            Bukkit.getLogger().severe("Failed to fetch player data for " + Bukkit.getPlayer(uuid));
+        }
+    }
+}
